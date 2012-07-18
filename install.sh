3df21464843e1b52b6184cbddc9fdd4b2649e64c
Angel Ramboi <angel.ramboi@gmail.com>
2012-07-18 11:32:48 +0300

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+if [ -d ~/.dotfiles ]; then
+    cd ~/.dotfiles
+    git pull origin master
+else
+    git clone git://github.com/limpangel/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+    for f in _*; do
+        link_file $f
+    done
+fi
+
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
